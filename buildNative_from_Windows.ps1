2ad39278edeb2d49a4d2e38801d8c12cc1186a82
Paul Hoguth <paul.hoguth@firstwaters.de>
2024-04-13 17:32:23 +0200

    removed walk-native for -O0 no-optimization

--- buildNative_from_Windows.ps1
+++ buildNative_from_Windows.ps1
@@ -1,19 +1,22 @@
 echo "Setting working dir to current script location: " $PSScriptRoot
 cd $PSScriptRoot
 echo "Starting Maven Build to single Jar file."
-mvn -B -ntp clean install -Pnative native:compile package "-Djava.awt.headless=false" "-Dmaven.tests.skip=true" "-Dorg.slf4j.simpleLogger.defaultLogLevel=WARN"
+mvn -B -ntp clean install compile -Pnative package "-Djava.awt.headless=false" "-Dmaven.tests.skip=true" "-Dorg.slf4j.simpleLogger.defaultLogLevel=WARN"
+echo "Generated contents in target:"
+ls .\target
 echo "Creating target-native dir"
 New-Item -ItemType Directory -Force -Path target-native
 echo "Cleaning target-native directory"
 rm target-native\*
 echo "Starting Native Build on Windows."
 native-image `
-    -g `
+    -g -O0 `
     "-Djava.awt.headless=false" `
     --enable-http `
     --enable-https `
     "-H:+UnlockExperimentalVMOptions" `
     "-H:ResourceConfigurationFiles=native-resources-configuration.json" `
-    -march=native `
     -jar .\target\SimpleInternetMonitor.jar `
     .\target-native\SimpleInternetMonitor
+echo "Generated contents in target-native:"
+ls .\target-native
