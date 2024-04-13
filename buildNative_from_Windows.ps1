5ce752f7062ca37df14814543f679b3217e6d1fc
Paul Hoguth <paul.hoguth@firstwaters.de>
2024-04-13 15:21:24 +0200

    Seperated MSVC relevant code for executing after Graal Build, avoiding $Path Problems.

--- buildNative_from_Windows.ps1
+++ buildNative_from_Windows.ps1
@@ -15,7 +15,3 @@ native-image `
     -march=native `
     -jar .\target\SimpleInternetMonitor.jar `
     .\target-native\SimpleInternetMonitor
-# Use the 64 bit version of editbin to avoid opening a console when executing the .exe
-# Install Instructions: https://stackoverflow.com/questions/57207503/dumpbin-exe-editbin-exe-package-needed-in-visual-studio-2019
-editbin.exe /SUBSYSTEM:WINDOWS .\target-native\SimpleInternetMonitor.exe
-makensis.exe .\NSIS_windows.nsi
