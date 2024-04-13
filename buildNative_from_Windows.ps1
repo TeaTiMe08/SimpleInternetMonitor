f2d93520e6c01434b6fe58d59fe1516ddd8298fb
Paul Hoguth <paul.hoguth@firstwaters.de>
2024-04-13 16:56:04 +0200

    -Pnative native:compile instead

--- buildNative_from_Windows.ps1
+++ buildNative_from_Windows.ps1
@@ -1,7 +1,7 @@
 echo "Setting working dir to current script location: " $PSScriptRoot
 cd $PSScriptRoot
 echo "Starting Maven Build to single Jar file."
-mvn -B -ntp clean install compile package "-Djava.awt.headless=false" "-Dmaven.tests.skip=true" "-Dorg.slf4j.simpleLogger.defaultLogLevel=WARN"
+mvn -B -ntp clean install -Pnative native:compile package "-Djava.awt.headless=false" "-Dmaven.tests.skip=true" "-Dorg.slf4j.simpleLogger.defaultLogLevel=WARN"
 echo "Creating target-native dir"
 New-Item -ItemType Directory -Force -Path target-native
 echo "Cleaning target-native directory"
