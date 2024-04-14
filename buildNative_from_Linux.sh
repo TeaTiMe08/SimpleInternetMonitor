4daf7ecb01f6c748b920b1478f30d137c7132b20
Paul Hoguth <paul.hoguth@firstwaters.de>
2024-04-14 02:08:52 +0200

    added no fallback to ubuntu version

--- buildNative_from_Linux.sh
+++ buildNative_from_Linux.sh
@@ -4,7 +4,7 @@ cd "$script_dir"
 echo "Starting Maven Build to single Jar file."
 mvn -B -ntp clean install compile package "-Djava.awt.headless=false" "-Dmaven.tests.skip=true" "-Dorg.slf4j.simpleLogger.defaultLogLevel=WARN"
 echo "Generated contents in target:"
-ls target/
+ls -alh target/
 echo "Creating target-native dir"
 mkdir -p target-native
 echo "Cleaning target-native directory"
@@ -13,9 +13,10 @@ echo "Starting Native Build on Linux."
 native-image \
     -g -O0 \
     "-Djava.awt.headless=false" \
+    --no-fallback --link-at-build-time \
     --enable-http \
     --enable-https \
     "-H:ResourceConfigurationFiles=native-resources-configuration.json" \
     -jar ./target/SimpleInternetMonitor.jar \
     ./target-native/SimpleInternetMonitor
-ls target-native/
+ls -alh target-native/
