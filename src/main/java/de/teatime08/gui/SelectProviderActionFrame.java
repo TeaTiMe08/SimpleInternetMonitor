64ff0123874b9edef6dc5e23a6c1c5b2d40e9599
Paul Hoguth <paul.hoguth@firstwaters.de>
2024-05-22 00:10:12 +0200

    Added Text Filter

--- src/main/java/de/teatime08/gui/SelectProviderActionFrame.java
+++ src/main/java/de/teatime08/gui/SelectProviderActionFrame.java
@@ -6,11 +6,15 @@ import de.teatime08.netlatency.protocols.IRequestCheckerProvider;
 import de.teatime08.util.StackTracePrinter;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.regex.PatternSyntaxException;
 
 public class SelectProviderActionFrame extends JDialog {
     private final SelectProviderActionFrame thus = this; // stupid swing sometimes needs th...
@@ -57,6 +61,17 @@ public class SelectProviderActionFrame extends JDialog {
         JScrollPane scrollPane = new JScrollPane(itemTable);
         add(scrollPane, BorderLayout.CENTER);
 
+        // create search filter
+        JTextField searchTextField = new JTextField();
+        searchTextField.setPreferredSize(new Dimension(150, 23));
+        SelectProviderActionFrame.AddFilter(itemTable, searchTextField);
+
+        JPanel searchPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        searchPanel.add(new JLabel("Search (regex):"));
+        searchPanel.add(searchTextField);
+
+        add(searchPanel, BorderLayout.NORTH);
+
         // Create buttons
         selectButton = new JButton("Select");
 
@@ -84,6 +99,46 @@ public class SelectProviderActionFrame extends JDialog {
                 } else {
                     JOptionPane.showMessageDialog(null, "Please select a Provider first.");
                 }
+            }
+        });
+    }
+
+    public static void AddFilter(JTable tbl, JTextField txtSearch){ //, Integer SearchColumnIndex) {
+
+        DefaultTableModel model = (DefaultTableModel) tbl.getModel();
+
+        final TableRowSorter< DefaultTableModel> sorter = new TableRowSorter< DefaultTableModel>(model);
+        tbl.setRowSorter(sorter);
+
+        txtSearch.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                OnChange();
+            }
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                OnChange();
+            }
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                OnChange();
+            }
+            public void OnChange() {
+                String txt = txtSearch.getText().toLowerCase();
+                if (txt.length() == 0) {
+                    sorter.setRowFilter(null);
+                } else {
+                    try {
+                        final int rowsCount = tbl.getModel().getRowCount();
+                        int[] rowIndices = new int[rowsCount];
+                        for (int i = 0; i < rowIndices.length; i++)
+                            rowIndices[i] = i;
+                        sorter.setRowFilter(RowFilter.regexFilter("^(?i)" + txt, rowIndices));
+                    } catch (PatternSyntaxException pse) {
+                        System.out.println("Bad regex pattern");
+                    }
+                }
+
             }
         });
     }
